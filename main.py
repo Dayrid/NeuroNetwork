f4f7a169c47c4475de915918353a8a7bee2caefa
Dayrid <dayrid69@yandex.ru>
2021-09-23 17:21:30 +0500

    New version

--- main.py
+++ main.py
@@ -2,15 +2,32 @@ import configparser
 import json
 import SQL
 import restore_data
+import pandas as pd
+import numpy as np
+
+pd.options.display.max_columns = None
+pd.options.display.max_rows = None
 
 
 class Preprocessing:
     def __init__(self):
         self.params = self.config('Settings.ini')
         self.params['selectedcols'] = self.params['selectedcols'].split(',')
-        sql = SQL.SQL(self.params['hydropost'])
-        df = sql.df
+        if self.params['sql_reading'].lower() == 'on':
+            sql = SQL.SQL(self.params['hydropost'])
+            df = sql.df
+        else:
+            df = self.xlsx_read(self.params['pathtofile'])
         restoring = restore_data.DataRestore(df, self.params)
+        raw_data = restoring.raw_data
+        print(raw_data)
+
+    def xlsx_read(self, filename):
+        # Чтение из xlsx формата
+        dfs = pd.read_excel(filename, sheet_name='Уровни', engine='openpyxl')
+        dfs = dfs[dfs['Код поста'] == int(self.params['hydropost'])]
+        dfs = dfs.sort_values('Дата - время')
+        return dfs
 
     @staticmethod
     def config(name):
--- restore_data.py
+++ restore_data.py
@@ -7,13 +7,17 @@ from restore_methods import naiveBayes, imputers
 class DataRestore:
     def __init__(self, df, params):
         df = self.cutting(df, params)
-        if params['restore_data'] != 'Off':
+        new_df = df
+        if params['restore_data'].lower() != 'off':
             missed_date_df = self.fill_missed_date(df, params)
             if params['restore_data'] == 'naive_bayes':
                 new_df = self.naive_bayes_restoring(missed_date_df, params)
             elif params['restore_data'] in ['knn', 'iter', 'mean']:
                 new_df = self.imputers_restoring(missed_date_df, params, params['restore_data'], 5)
-            print(new_df)
+            self.raw_data = new_df
+        else:
+            print('Восстановление данных отключено.')
+            self.raw_data = new_df
 
     @staticmethod
     def cutting(df, params):
@@ -37,21 +41,19 @@ class DataRestore:
                 df = df.set_index('Дата - время')
                 df = df.reindex(pd.date_range(m1, m2)).fillna(np.nan)
                 df = df.reset_index()
-                df.rename(columns={'index':'Дата - время'}, inplace=True)
+                df.rename(columns={'index': 'Дата - время'}, inplace=True)
             else:
                 df = df.set_index('Дата - время').fillna(np.nan)
                 df = df.reset_index()
-                df = df.rename(columns={'index':'Дата - время'}, inplace=True)
+                df = df.rename(columns={'index': 'Дата - время'}, inplace=True)
             return df
-
 
     @staticmethod
     def naive_bayes_restoring(df, params):
         obj = naiveBayes.NaiveBayes(df, params)
         return obj.restored_df
 
-
     @staticmethod
-    def imputers_restoring(df, params, method, k = None):
+    def imputers_restoring(df, params, method, k=None):
         obj = imputers.Imputers(df, params, method, k)
         return obj.restored_df
