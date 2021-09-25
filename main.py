0f1637aacbed4b333ea578e1effea3055b72cba4
Dayrid <dayrid69@yandex.ru>
2021-09-25 15:20:58 +0500

    New version

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class Preprocessing:
     def __init__(self):
         self.params = self.config('Settings.ini')
         self.params['selectedcols'] = self.params['selectedcols'].split(',')
+        self.params['selection_size'] = int(self.params['selection_size'])
+        self.params['predict_size'] = int(self.params['predict_size'])
         if self.params['sql_reading'].lower() == 'on':
             sql = SQL.SQL(self.params['hydropost'])
             df = sql.df
@@ -21,7 +23,9 @@ class Preprocessing:
             df = self.xlsx_read(self.params['pathtofile'])
         restoring = restore_data.DataRestore(df, self.params)
         self.raw_data = restoring.raw_data
+        # print(self.raw_data.tail(11))
         self.min_max = []
+        self.cube_formation()
 
     def xlsx_read(self, filename):
         # Чтение из xlsx формата
@@ -49,7 +53,46 @@ class Preprocessing:
                 self.min_max[1] - self.min_max[0]) + self.min_max[0]
 
     def cube_formation(self):
-        pass
+        """
+        Массивы выборки (массив x или multiple в прошлом PM):
+        selection_data: готовые данные выборки в numpy
+        selection_dates: массив дат идентичный массиву selection_data
+        selection_full_data: массив сшитых дат и данных
+        """
+        selection_data = []
+        selection_dates = []
+        selection_full_data = []
+        """
+        Массивы прогнозов (массив y или single в прошлом PM):
+        predict_data: готовые данные прогнозов в numpy
+        predict_dates: массив дат идентичный массиву predict_data
+        predict_full_data: массив сшитых дат и данных
+        """
+        predict_data = []
+        predict_dates = []
+        predict_full_data = []
+
+        df = self.raw_data
+        columns = self.params['selectedcols']
+
+        for i in range(len(df) - self.params['predict_size'] - self.params['selection_size'] + 1):
+            selection_dates.append(df.loc[i:i+self.params['selection_size']-1, 'Дата - время'].astype(str).values.tolist())
+            selection_data.append(df.loc[i:i+self.params['selection_size']-1, columns].values.tolist())
+            selection_full_data.append([[selection_dates[-1][j], selection_data[-1][j]] for j in range(self.params['selection_size'])])
+        selection_data = np.array(selection_data)
+        # print(selection_data)
+
+        for i in range(self.params['selection_size'], len(df) - self.params['predict_size'] + 1):
+            predict_dates.append(df.loc[i:i+self.params['predict_size']-1, 'Дата - время'].astype(str).values.tolist())
+            predict_data.append(df.loc[i:i+self.params['predict_size']-1, columns[0]].values.tolist())
+            predict_full_data.append([[predict_dates[-1][j], predict_data[-1][j]] for j in range(self.params['predict_size'])])
+        predict_data = np.array(predict_data)
+        # print(predict_data)
+
+        for i in range(len(selection_full_data)):
+            print(50*"-")
+            print(selection_full_data[i])
+            print(predict_full_data[i])
 
 
 a = Preprocessing()  # main1
