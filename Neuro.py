be0e460c01a871264953b5b458e43683e438b4f3
228FlashBack <kolechka.baskov.2000@mail.ru>
2021-10-17 16:46:56 +0500

    test

--- Neuro.py
+++ Neuro.py
@@ -9,23 +9,48 @@ import main
 
 
 class NeuroNetwork:
-
     data = main.Preprocessing()
 
-    def __init__(self, filename):
-        self.model = tf.keras.models.load_model(filename)
-        pass
+    def __init__(self, filename=None):
+        """
+        Конструктор класса нейросети.
+        :param filename: расположение файла с моделью нейросети
+        """
 
-    def __int__(self):
-        self.model = tf.keras.Sequential()
-        self.model.add(tf.keras.layers.Dense(30, input_shape=(5, 2), activation='linear'))
-        self.model.add(tf.keras.layers.SimpleRNN(150, activation='tanh'))
-        self.model.add(tf.keras.layers.SimpleRNN(50, activation='tanh'))
-        self.model.add(tf.keras.layers.Dense(5, activation='linear'))
+        if filename is not None:
+            self.model = tf.keras.models.load_model(filename)  # Создание модели через файл
+        else:
+            self.model = tf.keras.Sequential()  # Создание модели через конструктор
+
+            #   Добавление входного слоя
+            self.model.add(tf.keras.layers.Dense(30, input_shape=self.data.train_x.shape[1:], activation='linear'))
+
+            # Добавление скрытых слоёв
+            self.model.add(tf.keras.layers.SimpleRNN(150, activation='tanh', return_sequences=True))
+            self.model.add(tf.keras.layers.SimpleRNN(50, activation='tanh'))
+
+            # Выходной слой
+            self.model.add(tf.keras.layers.Dense(5, activation='linear'))
         pass
 
     def fit(self):
+        """
+        Обучение нейросети
+        """
+        train_x, train_y = self.data.train_x, self.data.train_y  # Создание тренировочной выборки
+
+        # Выбор функции ошибки и метода оптимизации градиентного спуска
+        self.model.compile(loss='mse', optimizer='Adam', metrics='mae')
+
+        # Обучение
+        history = self.model.fit(train_x, train_y, epochs=200, batch_size=10, validation_split=0.2)
+
         pass
 
-    def predict(self):
+    def predict(self, data):
+        return self.model.predict(data)
+
+    def test(self):
         pass
+
+
